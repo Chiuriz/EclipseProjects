91ceb5ba9d6f946ebf927f2fd50bc794b458e40d
Chiuriz <45494580+Chiuriz@users.noreply.github.com>
2018-12-11 17:17:00 +0100

    --

--- TileGame/src/it/rocco/tilegame/gfx/Assets.java
+++ TileGame/src/it/rocco/tilegame/gfx/Assets.java
@@ -1,10 +1,15 @@
 package it.rocco.tilegame.gfx;
 
+import java.awt.image.BufferedImage;
+
 public class Assets {
 
+	public static BufferedImage player, dirt, grass, stone, tree;
+	
 	public static void init () {
 		
-		
+		//creo un oggetto della classe "SpriteSheet" e gli passo come parametro il metodo di caricamento immagini della classe "ImageLoader"
+		SpriteSheet sheet = new SpriteSheet (ImageLoader.loadImage("/textures/sheet2"));
 		
 	}
 	
