85b133b16098ac4c1ea83521da95b38a51699b36
Chiuriz <Utente@Utente-PC>
2018-12-14 14:43:53 +0100

    ---

--- CorsoJava/src/it/corso/java/CorsoJava.java
+++ CorsoJava/src/it/corso/java/CorsoJava.java
@@ -7,18 +7,19 @@ public class CorsoJava {
 	public String s3;
 	public int i;
 	
-	
-	
 	public void esegui () {		
 		s3 = s1.concat(s2);
 		s3 = s3.replaceAll("A", " ");
-		
+		s3 = s3.replaceAll("E", " ");
+		s3 = s3.replaceAll("I", " ");
+		s3 = s3.replaceAll("O", " ");
+		s3 = s3.replaceAll("U", " ");
+			
 		for (i=0; i < s3.length(); i++) {
 			System.out.print(s3.charAt(i));
 		}
 	}	
 	
-
 	public static void main(String[] args) {
 		CorsoJava es = new CorsoJava();
 		es.esegui();	
