f8b066c432c6c48ccc4aceab5b81b4d2ee992a56
Chiuriz <Utente@Utente-PC>
2018-12-14 14:42:33 +0100

    --

--- CorsoJava/src/it/corso/java/CorsoJava.java
+++ CorsoJava/src/it/corso/java/CorsoJava.java
@@ -1,11 +1,26 @@
 package it.corso.java;
 
 public class CorsoJava {
+	
+	public String s1 = "ROCCO BELLI ABITA A SETTIMO DI MONTALTO UFFUGO";
+	public String s2 = " E LAVORA A COSENZA IN VIA F. E G. FALCONE";
+	public String s3;
+	public int i;
+	
+	
+	
+	public void esegui () {		
+		s3 = s1.concat(s2);
+		s3 = s3.replaceAll("A", " ");
+		
+		for (i=0; i < s3.length(); i++) {
+			System.out.print(s3.charAt(i));
+		}
+	}	
+	
 
 	public static void main(String[] args) {
-			
-		
-		
-	}
-
+		CorsoJava es = new CorsoJava();
+		es.esegui();	
+	}	
 }
