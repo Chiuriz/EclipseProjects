7fbbcacc968394e4ab6b718414142d5e526fc4df
Chiuriz <45494580+Chiuriz@users.noreply.github.com>
2018-12-19 11:58:19 +0100

    ---

--- TileGame/src/it/rocco/tilegame/entities/creatures/Creature.java
+++ TileGame/src/it/rocco/tilegame/entities/creatures/Creature.java
@@ -6,8 +6,8 @@ public abstract class Creature extends Entity {
 	
 	public static final int DEFAULT_HEALT = 10;
 	public static final float DEFAULT_SPEED = 3.0f;
-	public static final int DEFAULT_CREATURE_WIDTH = 64,
-							DEFAULT_CREATURE_HEIGHT = 64;
+	public static final int DEFAULT_CREATURE_WIDTH = 45,
+							DEFAULT_CREATURE_HEIGHT = 45;
 	
 	protected int health;
 	protected float speed;
